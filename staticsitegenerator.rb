49e0287e86550e2603b7ee0e1e0730a41c6521c4
Ryan Nickel <ryan@ryannickel.com>
2025-11-10 11:53:29 -0500

    Update URL to version 0.0.8

--- staticsitegenerator.rb
+++ staticsitegenerator.rb
@@ -1,7 +1,7 @@
 class Staticsitegenerator < Formula
   desc "StaticSiteGenerator"
   homepage "https://github.com/mrnickel/StaticSiteGenerator"
-  url "https://github.com/mrnickel/StaticSiteGenerator/archive/refs/tags/v0.0.7.tar.gz"
+  url "https://github.com/mrnickel/StaticSiteGenerator/archive/refs/tags/v0.0.8.tar.gz"
   sha256 "8752a8cc13cf942b511818457cece7f8ac1325b0f778f95a99b640ba9f1c902c"
   license "Apache-2.0"
 
