8b2599511b5f0b7a35ff0e974a07fe49142c4cd9
Ryan Nickel <ryan@ryannickel.com>
2025-11-10 12:05:58 -0500

    Update StaticSiteGenerator to version 0.0.9

--- staticsitegenerator.rb
+++ staticsitegenerator.rb
@@ -1,11 +1,11 @@
 class Staticsitegenerator < Formula
   desc "StaticSiteGenerator"
   homepage "https://github.com/mrnickel/StaticSiteGenerator"
-  url "https://github.com/mrnickel/StaticSiteGenerator/archive/refs/tags/v0.0.8.tar.gz"
+  url "https://github.com/mrnickel/StaticSiteGenerator/archive/refs/tags/v0.0.9.tar.gz"
   sha256 "8752a8cc13cf942b511818457cece7f8ac1325b0f778f95a99b640ba9f1c902c"
   license "Apache-2.0"
 
-  depends_on "go@1.19" => :build
+  depends_on "go@1.25.4" => :build
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w")
